2730aa40d0ea6e2aabd4941595dfd15a0b02cebb
Raquel <raquel.mrti@gmail.com>
2020-12-24 23:44:43 +0100

    Finish all modal content, minor css fixes

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const gallery = document.querySelector(".gallery");
+/* const gallery = document.querySelector(".gallery");
 
 gallery.addEventListener("click", (e) => {
   const galleryImg = e.target.matches(".gallery__img");
@@ -14,6 +14,7 @@ gallery.addEventListener("click", (e) => {
   const modalClose = () => {
     document.body.style.overflowY = "scroll";
     modal.classList.remove("is-open");
+    modal.removeEventListener("click", modalClose);
   };
 
   closeButton.addEventListener("click", modalClose);
@@ -22,4 +23,43 @@ gallery.addEventListener("click", (e) => {
       modalClose();
     }
   });
+}); */
+
+const gallery = document.querySelector(".gallery");
+
+gallery.addEventListener("click", (e) => {
+  
+  const modal = e.target.parentNode.nextElementSibling;
+  const closeButton = modal.querySelector(".gallery__modal__icon-close");
+
+  const modalOpen = () => {
+    modal.classList.add("is-open");
+    document.body.style.overflowY = "hidden";
+    modal.scrollTo(0, 0);
+  };
+
+  const modalClose = () => {
+    document.body.style.overflowY = "scroll";
+    modal.classList.remove("is-open");
+    modal.removeEventListener("click", modalClose);
+  };
+
+  closeButton.addEventListener("click", modalClose);
+  closeButton.addEventListener("keypress", (e) => {
+    if (e.key === "Enter") {
+      modalClose();
+    }    
+  });
+
+  const galleryImg = e.target.matches(".gallery__img");
+  const galleryCap = e.target.matches(".gallery__caption");
+
+  if (galleryImg | galleryCap) {
+    modalOpen();
+
+    if (modal.scrollY > 0) {
+      modal.scrollTo(0, 0);
+    }
+  }
+
 });
