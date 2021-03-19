73d085be42191a2bbfb7c04add1413e1aac7f34f
Raquel <raquel.mrti@gmail.com>
2021-03-19 18:57:42 +0100

    Add: animations and other aesthetic details. Change: some pictures of project no. 7

--- script.js
+++ script.js
@@ -1,29 +1,39 @@
-/* const gallery = document.querySelector(".gallery");
+const mediaQuery = window.matchMedia('(max-width: 800px)');
 
-gallery.addEventListener("click", (e) => {
-  const galleryImg = e.target.matches(".gallery__img");
-  const galleryCap = e.target.matches(".gallery__caption");
-  const modal = e.target.parentNode.nextElementSibling;
-  const closeButton = modal.querySelector(".gallery__modal__icon-close");
+function handleDeviceChange(e) {
 
-  if (galleryImg | galleryCap) {
-    modal.classList.add("is-open");
-    document.body.style.overflowY = "hidden";
+
+  const nav = document.querySelector(".nav");
+    const activeLink = document.querySelector(".nav__active");
+    const sectionOne = document.querySelector(".header");
+
+  if (e.matches) {
+
+    const options = {
+      rootMargin: "45px 0px 0px 0px"
+    };
+
+    const headerObserver = new IntersectionObserver(function (entries, headerObserver) {
+
+      entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+          nav.style.backgroundColor = "#ff404e";
+          activeLink.classList.add("scrolled");
+        } else {
+          nav.style.backgroundColor = "#fdfdfd";
+          activeLink.classList.remove("scrolled");
+        }
+      })
+    }, options);
+
+    headerObserver.observe(sectionOne);
   }
+}
+  
+mediaQuery.addEventListener("change", handleDeviceChange);
+handleDeviceChange(mediaQuery);
 
-  const modalClose = () => {
-    document.body.style.overflowY = "scroll";
-    modal.classList.remove("is-open");
-    modal.removeEventListener("click", modalClose);
-  };
-
-  closeButton.addEventListener("click", modalClose);
-  closeButton.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-      modalClose();
-    }
-  });
-}); */
+//////// GALLERY ////////
 
 const gallery = document.querySelector(".gallery");
 
@@ -32,34 +42,32 @@ gallery.addEventListener("click", (e) => {
   const modal = e.target.parentNode.nextElementSibling;
   const closeButton = modal.querySelector(".gallery__modal__icon-close");
 
+  const modalClose = () => {
+    document.body.style.overflowY = "scroll";
+    closeButton.removeEventListener("click", modalClose);
+    modal.classList.remove("is-open");
+  };
+
   const modalOpen = () => {
+
     modal.classList.add("is-open");
     document.body.style.overflowY = "hidden";
     modal.scrollTo(0, 0);
+
+    closeButton.addEventListener("click", modalClose);
+    closeButton.addEventListener("keypress", (e) => {
+      if (e.key === "Enter") {
+        modalClose();
+      }    
+    });
   };
 
-  const modalClose = () => {
-    document.body.style.overflowY = "scroll";
-    modal.classList.remove("is-open");
-    modal.removeEventListener("click", modalClose);
-  };
-
-  closeButton.addEventListener("click", modalClose);
-  closeButton.addEventListener("keypress", (e) => {
-    if (e.key === "Enter") {
-      modalClose();
-    }    
-  });
 
   const galleryImg = e.target.matches(".gallery__img");
   const galleryCap = e.target.matches(".gallery__caption");
 
   if (galleryImg | galleryCap) {
     modalOpen();
-
-    if (modal.scrollY > 0) {
-      modal.scrollTo(0, 0);
-    }
   }
-
 });
+
